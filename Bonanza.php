d333946bd490d67f0a7a9f1ebd611500c8a835a4
Emile Thé <emilethe123@gmail.com>
2020-01-28 11:04:02 -0500

    Initial commit

---
+++ Bonanza.php
@@ -0,0 +1,31 @@
+<html lang="en">
+<header>
+    <link rel="stylesheet" type="text/css" href="Style.css">
+</header>
+<body>
+<div id="header">
+    <h1 class="title">Header</h1>
+</div>
+<div id="body">
+    <h1 class="title"></h1>
+    <form>
+        <h1>Login:</h1>
+        <label>
+            <input type="text" name="Username" placeholder="Username">
+        </label>
+        <label>
+            <input type="password" name="Password" placeholder="Password">
+        </label>
+        <button class="button">Sign up</button>
+        <label>
+            <input type="submit" name="Login" class="button" value="Login">
+        </label>
+    </form>
+</div>
+<div id="footer">
+    <h1 class="title">Footer</h1>
+    <h4>Created by: Emile The</h4>
+    <h4>Contact info: theem1832502@etu.cegepjonquiere.ca</h4>
+</div>
+</body>
+</html>
